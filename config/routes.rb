ed7ab0199c67d8820847dc4b41a8f230a55bbb8d
Geoffrey Cheah <geoffcheah@gmail.com>
2018-03-05 17:44:38 +0000

    created relevant routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit, :update] do
+    resources :favourites, only: [:create, :destroy]
+  end
+
+  resources :routes, only: [:index, :show] do
+    resources :reviews, only: [:create]
+  end
 end
