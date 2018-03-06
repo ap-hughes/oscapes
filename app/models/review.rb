ce526dfb1848dcf0cbaa45967bfcf7e03d448149
Alex Hughes <aphughes@talk21.com>
2018-03-06 12:04:15 +0000

    Completed seed and added some validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :route
   belongs_to :user
+
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: ["1", "2", "3", "4", "5"] }
 end
