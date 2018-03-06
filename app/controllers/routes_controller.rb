4155460f5bfa930c28b74a37e54c1fb5d7af18e3
Kirsty Leishman <leishman.kirsty@gmail.com>
2018-03-06 12:22:24 +0000

    Basic info on show page and icons for difficulty

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -5,5 +5,14 @@ class RoutesController < ApplicationController
 
   def show
     @route = Route.find(params[:id])
+    if @route.difficulty == "Hard"
+      @difficulty = 4
+    elsif @route.difficulty == "Medium"
+      @difficulty = 3
+    elsif @route.difficulty == "Low"
+      @difficulty = 2
+    else
+      @difficulty = 1
+    end
   end
 end
