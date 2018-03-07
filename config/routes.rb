23a5f226c2edf06165b3f360bbdc9e3a0d049f64
Kirsty Leishman <leishman.kirsty@gmail.com>
2018-03-07 16:29:09 +0000

    Users can add a route to their favourites

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :edit, :update] do
-    resources :favourites, only: [:create, :destroy]
-  end
+  resources :users, only: [:show, :edit, :update]
 
   resources :routes, only: [:index, :show] do
     resources :reviews, only: [:create]
+    resources :favourites, only: [:create, :destroy]
   end
 end
