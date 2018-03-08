b3c786f26ee1f3087f15c91a711966a46a9e7a08
Geoffrey Cheah <geoffcheah@gmail.com>
2018-03-08 18:44:07 +0000

    ajax reviews works

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,20 @@
 class ReviewsController < ApplicationController
   def create
     @route = Route.find(params[:route_id])
-    @user = current_user
     @review = Review.new(review_params)
     authorize @review
     @review.route = @route
-    @review.user = @user
+    @review.user = current_user
     if @review.save
-      redirect_to route_path(@route)
+      respond_to do |format|
+        format.html { redirect_to route_path(@route) }
+        format.js
+      end
     else
-      render 'reviews/show'
+      respond_to do |format|
+        format.html { render 'reviews/show' }
+        format.js
+      end
     end
   end
 
