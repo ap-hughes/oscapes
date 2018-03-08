676e44313da8e0cc8faa0d71ecf6c6aefcf5924e
Geoffrey Cheah <geoffcheah@gmail.com>
2018-03-08 15:50:01 +0000

    added review route function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,21 @@
 class ReviewsController < ApplicationController
   def create
+    @route = Route.find(params[:route_id])
+    @user = current_user
+    @review = Review.new(review_params)
+    authorize @review
+    @review.route = @route
+    @review.user = @user
+    if @review.save
+      redirect_to route_path(@route)
+    else
+      render 'reviews/show'
+    end
+  end
 
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
