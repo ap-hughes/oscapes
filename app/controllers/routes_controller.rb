66a04e8844c9f8e4ce7a993a765eba5f4b8f5e0c
Kirsty Leishman <leishman.kirsty@gmail.com>
2018-03-09 15:24:58 +0000

    edit route path created, image tags added, get_images route created toget suggested images - js to be added to make it on one page

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,16 +1,17 @@
 class RoutesController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :find_route, only: [:show, :edit, :update]
   def index
     @routes = policy_scope(Route).order(created_at: :desc)
   end
 
   def show
-    @route = Route.find(params[:id])
     authorize @route
     @review = Review.new
     @difficulty = get_difficulty_level
     # @coordinates = @route.coordinates.order(id: :desc).map { |coordinate| [coordinate.longitude, coordinate.latitude] }
     @coordinates = coordinates_from(@route.coordinates)
+    @image_coordinates = get_image_coordinates(@coordinates)
     @center = find_center(@coordinates)
     if !@route.image_gallery_1
       get_images(@coordinates)
@@ -18,8 +19,50 @@ class RoutesController < ApplicationController
     @images = [@route.hero_image, @route.image_gallery_1, @route.image_gallery_2]
   end
 
+  def edit
+    authorize @route
+    @coordinates = coordinates_from(@route.coordinates)
+    @image_coordinates = get_image_coordinates(@coordinates)
+  end
+
+  def update
+    @route.update(route_params)
+    redirect_to route_path(@route)
+  end
+
+  def get_image
+    @route = Route.find(params[:id])
+    authorize @route
+    @coordinates = coordinates_from(@route.coordinates)
+    @image_coordinates = get_image_coordinates(@coordinates)
+    search_terms = {
+      lat: @image_coordinates[0][1],
+      lon: @image_coordinates[0][0],
+      radius: 0,
+      has_geo: true,
+      content_type: 1,
+      tags: ['nature', 'landscape'],
+      tag_mode: 'any',
+      per_page: 10,
+    }
+
+    FlickRaw.api_key = ENV['flickr_api_key']
+    FlickRaw.shared_secret = ENV['flickr_api_secret']
+
+    list = flickr.photos.search(search_terms)
+    number = (0..9).to_a.sample
+    @image = FlickRaw.url_c(list[number])
+  end
 
   private
+
+  def find_route
+    @route = Route.find(params[:id])
+  end
+
+  def route_params
+    params.require(:route).permit(:name, :hero_image, :image_gallery_1, :image_gallery_2)
+  end
 
   def get_difficulty_level
     if @route.difficulty == "Hard"
@@ -33,16 +76,19 @@ class RoutesController < ApplicationController
     end
   end
 
-  def get_images(coordinates)
-    image_gallery = []
+  def get_image_coordinates(coordinates)
     if @route.coordinates.start_with?("[[[")
       array_number = coordinates.length / 2
       middle_coordinate = coordinates[array_number].length / 2
-      image_coordinates = [coordinates.first.first, coordinates[array_number][middle_coordinate], coordinates.last.last]
+      @image_coordinates = [coordinates.first.first, coordinates[array_number][middle_coordinate], coordinates.last.last]
     else
       middle_coordinate = coordinates.length / 2
-      image_coordinates = [coordinates.first, coordinates[middle_coordinate], coordinates.last]
+      @image_coordinates = [coordinates.first, coordinates[middle_coordinate], coordinates.last]
     end
+  end
+
+  def get_images(image_coordinates)
+    image_gallery = []
 
     image_coordinates.each do |coordinate|
       search_terms = {
@@ -51,7 +97,8 @@ class RoutesController < ApplicationController
         radius: 0,
         has_geo: true,
         content_type: 1,
-        # geo_context: 2,
+        tags: ['nature', 'landscape'],
+        tag_mode: 'any',
         per_page: 10,
       }
 
--- app/policies/route_policy.rb
+++ app/policies/route_policy.rb
@@ -4,4 +4,17 @@ class RoutePolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    # record.user_id == user
+    true
+  end
+
+  def get_image?
+    update?
+  end
 end
