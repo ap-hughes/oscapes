2262cbe867a9c2a34d90fb5c2161def12a89c294
Geoffrey Cheah <geoffcheah@gmail.com>
2018-03-12 13:14:30 +0000

    added a point to the map and created model for interest point and ran the migration

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,6 +2,7 @@ class Route < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :favourites, dependent: :destroy
+  has_many :interest_points
 
   validates :name, presence: true
   validates :description, presence: true
